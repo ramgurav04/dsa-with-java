8010a51cd21de11214077ea4d0afa0e40e0b49e0
RAM GURAV <ramgurav487@gmail.com>
2026-09-06 20:27:55 +0530

    movezerotoend solved

---
+++ 000001Array/NEED20/movezerotoend.java
@@ -0,0 +1,27 @@
+class movezerotoend{
+    public static void moveZeroToEnd(int arr[]) {
+        int n = arr.length;
+        int count = 0; // Count of non-zero elements
+
+        // Traverse the array. If element encountered is non-zero, then
+        // replace the element at index 'count' with this element
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i]; // here count is incremented
+            }
+        }
+
+        // Now all non-zero elements have been shifted to front and 'count' is
+        // set as index of first 0. Make all elements 0 from count to end.
+        while (count < n) {
+            arr[count++] = 0;
+        }
+    }
+    public static void main(String[] args) {
+        int arr [] = {1, 0, 2, 0, 3, 4, 0, 5};
+        moveZeroToEnd(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
